7c2c5a531a3b99cc94e438772b7aa7bff5426a8e
Yug Patel <yugpathik7@gmail.com>
2025-09-16 15:52:43 -0400

    .

--- frontend/src/components/Town/TownGameScene.ts
+++ frontend/src/components/Town/TownGameScene.ts
@@ -421,7 +421,7 @@ export default class TownGameScene extends Phaser.Scene {
         font: '18px monospace',
         color: '#000000',
         // padding: {x: 20, y: 10},
-        backgroundColor: '#3bf534ff',
+        backgroundColor: '#3bf534ff', // green: 3bf534ff
       })
       .setDepth(6);
     this.coveyTownController.ourPlayer.gameObjects = {
