932be92310ebafc1f63e5ab6b343ae9e8dd2399f
Yug Patel <yugpathik7@gmail.com>
2025-09-18 23:36:49 -0400

    partially completed QuantumTicTacToeGame.ts

---
+++ townService/src/town/games/QuantumTicTacToeGame.ts
@@ -0,0 +1,168 @@
+import {
+  GameMove,
+  QuantumTicTacToeGameState,
+  QuantumTicTacToeMove,
+} from '../../types/CoveyTownSocket';
+import Game from './Game';
+import TicTacToeGame from './TicTacToeGame';
+import Player from '../../lib/Player';
+import InvalidParametersError, {
+  GAME_FULL_MESSAGE,
+  PLAYER_ALREADY_IN_GAME_MESSAGE,
+  PLAYER_NOT_IN_GAME_MESSAGE,
+} from '../../lib/InvalidParametersError';
+
+/**
+ * A QuantumTicTacToeGame is a Game that implements the rules of the Tic-Tac-Toe variant described at https://www.smbc-comics.com/comic/tic.
+ * This class acts as a controller for three underlying TicTacToeGame instances, orchestrating the "quantum" rules by taking
+ * the role of the monitor.
+ */
+export default class QuantumTicTacToeGame extends Game<
+  QuantumTicTacToeGameState,
+  QuantumTicTacToeMove
+> {
+  // private _games: { A: TicTacToeGame; B: TicTacToeGame; C: TicTacToeGame };
+
+  private _xScore: number = 0;
+
+  private _oScore: number = 0;
+
+  private _moveCount: number = 0;
+
+  public constructor() {
+    // TODO: implement me
+    super({
+      moves: [],
+      status: 'WAITING_TO_START',
+      xScore: 0,
+      oScore: 0,
+      publiclyVisible: {
+        A: [
+          [false, false, false],
+          [false, false, false],
+          [false, false, false],
+        ],
+        B: [
+          [false, false, false],
+          [false, false, false],
+          [false, false, false],
+        ],
+        C: [
+          [false, false, false],
+          [false, false, false],
+          [false, false, false],
+        ],
+      },
+    });
+  }
+
+  protected _join(player: Player): void {
+    if (this.state.x === player.id || this.state.o === player.id) {
+      throw new InvalidParametersError(PLAYER_ALREADY_IN_GAME_MESSAGE);
+    }
+    // updates state with x = playerId if x is not already taken
+    if (!this.state.x) {
+      this.state = {
+        ...this.state,
+        x: player.id,
+      };
+      //else updates o with player id
+    } else if (!this.state.o) {
+      this.state = {
+        ...this.state,
+        o: player.id,
+      };
+    } else {
+      throw new InvalidParametersError(GAME_FULL_MESSAGE);
+    }
+    //Updates status to in progress
+    if (this.state.x && this.state.o) {
+      this.state = {
+        ...this.state,
+        status: 'IN_PROGRESS',
+      };
+    }
+  }
+
+  protected _leave(player: Player): void {
+    if (this.state.x !== player.id && this.state.o !== player.id) {
+      throw new InvalidParametersError(PLAYER_NOT_IN_GAME_MESSAGE);
+    }
+    // Handles case where the game has not started yet by checking if x or o player are undefined (default)
+    if (this.state.o === undefined) {
+      this.state = {
+        moves: [],
+        status: 'WAITING_TO_START',
+        xScore: 0,
+        oScore: 0,
+        publiclyVisible: {
+          A: [
+            [false, false, false],
+            [false, false, false],
+            [false, false, false],
+          ],
+          B: [
+            [false, false, false],
+            [false, false, false],
+            [false, false, false],
+          ],
+          C: [
+            [false, false, false],
+            [false, false, false],
+            [false, false, false],
+          ],
+        },
+      };
+      return;
+    }
+    
+    //Incase someone leaves remaining player wins 
+    if (this.state.x === player.id) {
+      this.state = {
+        ...this.state,
+        status: 'OVER',
+        winner: this.state.o,
+      };
+    } else {
+      this.state = {
+        ...this.state,
+        status: 'OVER',
+        winner: this.state.x,
+      };
+    }
+  }
+
+  /**
+   * Checks that the given move is "valid": that the it's the right
+   * player's turn, that the game is actually in-progress, etc.
+   * @see TicTacToeGame#_validateMove
+   */
+  private _validateMove(move: GameMove<QuantumTicTacToeMove>): void {
+    // TODO: implement me
+  }
+
+  public applyMove(move: GameMove<QuantumTicTacToeMove>): void {
+    this._validateMove(move);
+
+    // TODO: implement the guts of this method
+
+    this._checkForWins();
+    this._checkForGameEnding();
+  }
+
+  /**
+   * Checks all three sub-games for any new three-in-a-row conditions.
+   * Awards points and marks boards as "won" so they can't be played on.
+   */
+  private _checkForWins(): void {
+    // TODO: implement me
+  }
+
+  /**
+   * A Quantum Tic-Tac-Toe game ends when no more moves are possible.
+   * This happens when all squares on all boards are either occupied or part of a won board.
+   */
+  private _checkForGameEnding(): void {
+    // TODO: implement me
+  }
+}
